ad053ed4f1301aebad1851dfcefe79ea382f0e1b
Yaxin Cheng <yx299612@dal.ca>
2016-08-24 08:51:21 +0800

    Add GCD to city loader

--- YahooWeatherSource/CityLoader.swift
+++ YahooWeatherSource/CityLoader.swift
@@ -10,61 +10,83 @@ import Foundation
 
 public struct CityLoader: WeatherSourceProtocol {
 
+	let queue: dispatch_queue_t
 	public init() {
+		queue = dispatch_queue_create("CityLoaderQueue", nil)
 	}
 	
 	public func loadCity(cityName: String, complete: ([NSDictionary]) -> Void) {
-		let baseSQL: WeatherSourceSQLPatterns = .city
-		let sql = baseSQL.generateSQL(with: cityName)
-		sendRequst(sql) {
-			guard let citiesJSON = $0 as? NSDictionary else {
-				complete([])
-				return
+		dispatch_async(queue) {
+			let baseSQL: WeatherSourceSQLPatterns = .city
+			let sql = baseSQL.generateSQL(with: cityName)
+			self.sendRequst(sql) {
+				guard let citiesJSON = $0 as? NSDictionary else {
+					dispatch_async(dispatch_get_main_queue()) {
+						complete([])
+					}
+					return
+				}
+				let unwrapped: [NSDictionary]
+				if let places = (citiesJSON["query"] as? NSDictionary)?["results"]?["place"] as? [NSDictionary] {
+					unwrapped = places
+				} else if let place = (citiesJSON["query"] as? NSDictionary)?["results"]?["place"] as? NSDictionary {
+					unwrapped = [place]
+				} else {
+					dispatch_async(dispatch_get_main_queue()) {
+						complete([])
+					}
+					return
+				}
+				dispatch_async(dispatch_get_main_queue()) {
+					complete(unwrapped)
+				}
 			}
-			let unwrapped: [NSDictionary]
-			if let places = (citiesJSON["query"] as? NSDictionary)?["results"]?["place"] as? [NSDictionary] {
-				unwrapped = places
-			} else if let place = (citiesJSON["query"] as? NSDictionary)?["results"]?["place"] as? NSDictionary {
-				unwrapped = [place]
-			} else {
-				complete([])
-				return
-			}
-			complete(unwrapped)
 		}
 	}
 	
 	
 	public func daytime(for city: NSDictionary, complete: (NSDictionary?) -> Void) {
-		guard let woeid = city["woeid"] as? String else { return }
-		updateTime(woeid: woeid) {
-			guard $0 != nil && $1 != nil else {
-				complete(nil)
-				return
+		dispatch_sync(queue) {
+			guard let woeid = city["woeid"] as? String else { return }
+			self.updateTime(woeid: woeid) {
+				guard $0 != nil && $1 != nil else {
+					dispatch_async(dispatch_get_main_queue()) {
+						complete(nil)
+					}
+					return
+				}
+				var cityDictionary: Dictionary<String, AnyObject> = city as! Dictionary<String, AnyObject>
+				cityDictionary["sunrise"] = $0
+				cityDictionary["sunset"] = $1
+				complete(cityDictionary)
 			}
-			var cityDictionary: Dictionary<String, AnyObject> = city as! Dictionary<String, AnyObject>
-			cityDictionary["sunrise"] = $0
-			cityDictionary["sunset"] = $1
-			complete(cityDictionary)
 		}
 	}
 	
 	public func updateTime(woeid woeid: String, complete: (sunrise: NSDateComponents?, sunset: NSDateComponents?) -> Void ) {
-		let baseSQL = WeatherSourceSQLPatterns.daytime
-		let sql = baseSQL.generateSQL(with: woeid)
-		sendRequst(sql) {
-			guard
-				let daytimeJSON = $0 as? NSDictionary,
-				let unwrapped = ((daytimeJSON["query"] as? NSDictionary)?["results"]?["channel"] as? NSDictionary)?["astronomy"] as? NSDictionary,
-				let sunriseString = unwrapped["sunrise"] as? String,
-				let sunrise = NSDateComponents(from: sunriseString),
-				let sunsetString = unwrapped["sunset"] as? String,
-				let sunset = NSDateComponents(from: sunsetString)
-			else {
-				complete(sunrise: nil, sunset: nil)
-				return
+		dispatch_async(queue) {
+			let baseSQL = WeatherSourceSQLPatterns.daytime
+			let sql = baseSQL.generateSQL(with: woeid)
+			dispatch_sync(self.queue) {
+				self.sendRequst(sql) {
+					guard
+						let daytimeJSON = $0 as? NSDictionary,
+						let unwrapped = ((daytimeJSON["query"] as? NSDictionary)?["results"]?["channel"] as? NSDictionary)?["astronomy"] as? NSDictionary,
+						let sunriseString = unwrapped["sunrise"] as? String,
+						let sunrise = NSDateComponents(from: sunriseString),
+						let sunsetString = unwrapped["sunset"] as? String,
+						let sunset = NSDateComponents(from: sunsetString)
+					else {
+						dispatch_async(dispatch_get_main_queue()) {
+							complete(sunrise: nil, sunset: nil)
+						}
+						return
+					}
+					dispatch_async(dispatch_get_main_queue()) {
+						complete(sunrise: sunrise, sunset: sunset)
+					}
+				}
 			}
-			complete(sunrise: sunrise, sunset: sunset)
 		}
 	}
 }
